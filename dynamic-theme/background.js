089a43c708d7493d456523239b131ec96fdace68
Andy <amckay@mozilla.com>
2017-04-05 09:56:23 -0700

    add in colours

--- dynamic-theme/background.js
+++ dynamic-theme/background.js
@@ -1,5 +1,9 @@
 function handleClick() {
   browser.theme.update({
+    colors: {
+      accentcolor: "#d92215",
+      textcolor: "rgb(224, 41, 29)"
+    },
     images: {
       theme_frame: "images/clouds.jpg"
     }
